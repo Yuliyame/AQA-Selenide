d77d8defc0f196fd544fd3c794a29f4711fbe03e
YuliyaYuliya <yr555556666667777777@gmail.com>
2024-03-20 20:42:13 +0300

    initial commit

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,30 @@
+package ru.netology.selenide;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.concurrent.locks.Condition;
+
+public class AppCardDeliveryTaskOneTest {
+
+    private String generateDate (int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFornatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open ("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Рязань");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKays(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELITE...);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
+        $("[data-test-id='phone'] input").setValue("+78005553535");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(condition.exactText("стреча успешно забронирована на " + planningDate));
+    }
+}
