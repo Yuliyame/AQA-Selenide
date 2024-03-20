25125a413a782967084263a7439b5e116181e939
YuliyaYuliya <yr555556666667777777@gmail.com>
2024-03-20 21:27:56 +0300

    next commit

--- src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.locks.Condition;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class AppCardDeliveryTaskOneTest {
 
     private String generateDate (int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFornatter.ofPattern(pattern));
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
@@ -17,7 +21,7 @@ public class AppCardDeliveryTaskOneTest {
         open ("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Рязань");
         String planningDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKays(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELITE...);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELITE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
         $("[data-test-id='phone'] input").setValue("+78005553535");
@@ -25,6 +29,6 @@ public class AppCardDeliveryTaskOneTest {
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(condition.exactText("стреча успешно забронирована на " + planningDate));
+                .shouldHave(Condition.exactText("стреча успешно забронирована на " + planningDate));
     }
 }
